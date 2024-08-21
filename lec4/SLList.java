9ac6815cbca1aa52ccb4bb47109e0df67ad44ac8
noslanker <juice1750@outlook.com>
2024-08-21 13:20:52 +0800

    SLList is actually Single Linked List.
    Sentinel is head node before the first node used to ensure the list's security.

--- lec4/SLList.java
+++ lec4/SLList.java
@@ -5,56 +5,77 @@ public class SLList {
 
 
     private class IntNode {
-     public    int item;
-     public    IntNode next;
+        public int item;
+        public IntNode next;
 
-        IntNode(int i,IntNode n)
-        {
-            item=i;
-            next=n;
+        IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
         }
     }
 
-   private IntNode first;
+    /* The first node , if it exists. It's behind the sentinel. */
+    private IntNode sentinel;
+    private int size;
 
-    public  SLList(int x)
-    {
-        first =new IntNode(x,null);
+    public SLList(int x) {
+        size = 0;
+        sentinel = new IntNode(x, null);
+        size++;
     }
 
-    public  void addFirst(int x)
-    {
-        first = new IntNode(x,first);
+    public SLList() {
+        sentinel = new IntNode(0,null);
+        size = 0;
     }
 
-    public  int getFirst()
-    {
-        return  first.item;
+    public void addFirst(int x) {
+       sentinel.next = new IntNode(x,sentinel.next);
+       size++;
     }
 
-    /* Since there is no recursion in SLList. We need to create it! */
-    public int size()
-    {
-        return  size(first);
+    public int getFirst() {
+        return sentinel.item;
     }
 
-    private   int size(IntNode p)
-    {
-        if(p.next==null)
-            return 1;
-        else
-            return 1+size(p.next);
+    public void addLast(int x) {
+        IntNode p = sentinel;
+        while (p.next != null) p = p.next;
+        p.next = new IntNode(x, null);
+        size++;
     }
 
-    public  static  void main(String[] args)
-    {
+    // We want a  faster way to get the size by cache the size while we do insert or remove
+    public int size() {
+        return size;
+    }
 
-        SLList sll =new SLList(5);
+//    /* Since there is no recursion in SLList. We need to create it! */
+//    public int size() {
+//        return size(sentinel);
+//    }
+//
+//    private int size(IntNode p) {
+//        if (p.next == null)
+//            return 1;
+//        else
+//            return 1 + size(p.next);
+//    }
+
+    public static void main(String[] args) {
+
+     /*   SLList sll = new SLList(5);
 
         sll.addFirst(10);
         sll.addFirst(15);
         sll.addFirst(20);
 
-        System.out.println(sll.size());
+        System.out.println(sll.size());*/
+
+
+        SLList L = new SLList();
+        //L.addFirst(5);
+        L.addLast(10);
+        System.out.println(L.size());
     }
 }
