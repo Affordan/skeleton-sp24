afd2ce6888115a2fa960cfd5aecffbd189879904
noslanker <juice1750@outlook.com>
2024-08-24 19:50:42 +0800

    lab02 : completed 3 exercies.
    Actually to figure out what I need to do in this lab and set break point to debug these code cost me a lot of time. I even thought it was very boring.

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,16 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            String password="39291226";
+            b.phase0(password);
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            IntList password = IntList.of(0,9,3,0,8);
+            b.phase1(password); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+
+            b.phase2("-81201430");
         }
     }
 }
