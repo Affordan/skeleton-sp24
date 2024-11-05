5124382ef6ecd5a4698c3c3020ace566681818b9
Affordan <2310460@mail.nankai.edu.cn>
2024-11-05 10:15:54 +0800

    implement iterator methods and define SpecificIterator class.
    cs61B is wonderful course! I was striken when I rewatch the video second times!

--- proj1c/src/deque/ArrayDeque61B.java
+++ proj1c/src/deque/ArrayDeque61B.java
@@ -1,5 +1,261 @@
 package deque;
 
-public class ArrayDeque61B<T> implements Deque61B<T> {
+import com.google.common.truth.Truth;
+import net.sf.saxon.expr.Component;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.lang.Math.*;
+
+public class ArrayDeque61B<T> implements Deque61B<T>,Iterable<T> {
+
+    private int size_;
+    private int capacity;
+    private T[] items;
+    private int head;
+    private int tail;
+    private int middle;
+
+    public ArrayDeque61B() {
+        size_ = 0;
+        capacity = 8;
+        items = (T[]) new Object[8];
+        middle = capacity / 2;
+        head = middle - 1;   //let head in the middle of the array.
+        tail = middle;
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(T x) {
+        items[head] = x;
+        head = Math.floorMod(head - 1, capacity);
+        size_++;
+        if (isFull())
+            resize();
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(T x) {
+        items[tail] = x;
+        tail = Math.floorMod(tail + 1, capacity);
+        size_++;
+        if (isFull())
+            resize();
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List<T> toList() {
+        List<T> li = new ArrayList<>();
+        int tmp = head;
+        while (true) {
+            tmp = Math.floorMod(tmp + 1, capacity);
+            if (tmp == tail) break;
+            li.add(items[tmp]);
+        }
+        return li;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size_ == 0;
+    }
+
+    /**
+     * Returns if the deque is full.
+     *
+     * @return {@code true} if the deque is full,{@code false}  otherwise.
+     */
+    private boolean isFull() {
+        //return head==tail;  //while head equals tail,the deque has no empty place.
+        return size_ == capacity;
+    }
+
+    /**
+     * If the deque is full, expand it to a double size deque.
+     * <p>
+     * A tricky problem is to copy data appropriately to new array.
+     *
+     * @author Affordan
+     */
+
+    private void resize() {
+        int newCapacity = 2 * capacity;
+        int newMiddle = newCapacity / 2;
+        int newHead = newMiddle - 1;
+        int newTail = newMiddle;
+        T[] newItems = (T[]) new Object[newCapacity];
+
+        int tmp = middle - 1;
+
+        /* iterate from middle to head*/
+        while (tmp != head) {
+            newItems[newHead] = items[tmp];
+            tmp = Math.floorMod(tmp - 1, capacity);
+            /*Actually  just write head-- is enough
+             * cause the total capacity is 2 times of original capacity.
+             * */
+            newHead = Math.floorMod(newHead - 1, newCapacity);
+
+        }
+        /* iterate from middle to tail*/
+        tmp = middle;
+        while (tmp != tail) {
+            newItems[newTail] = items[tmp];
+            tmp = Math.floorMod(tmp + 1, capacity);
+            newTail = Math.floorMod(newTail + 1, newCapacity);
+
+        }
+
+        capacity = newCapacity;
+        middle = newMiddle;
+        head = newHead;
+        tail = newTail;
+        items = newItems;
+    }
+
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size_;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeFirst() {
+        head = Math.floorMod(head + 1, capacity);
+        T res = items[head];
+        items[head] = null;
+        size_--;
+
+        return res;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public T removeLast() {
+        tail = Math.floorMod(tail - 1, capacity);
+        T res = items[tail];
+        items[tail] = null;
+        size_--;
+
+        return res;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T get(int index) {
+        int tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = Math.floorMod(tmp + 1, capacity);
+        }
+
+        return items[tmp];
+    }
+    private class SpecificIterator implements Iterator<T>
+    {
+        int wizPos;
+        SpecificIterator()
+        {
+            wizPos=0;
+        }
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return wizPos<size_;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            T returnItem=items[wizPos];
+            wizPos++;
+            return returnItem;
+        }
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new SpecificIterator();
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    @Override
+    public  boolean equals(Object obj)
+    {
+
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
 }
+
--- proj1c/src/deque/LinkedListDeque61B.java
+++ proj1c/src/deque/LinkedListDeque61B.java
@@ -1,5 +1,231 @@
 package deque;
 
-public class LinkedListDeque61B<T> implements Deque61B<T> {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class LinkedListDeque61B<T> implements Deque61B<T>,Iterable<T> {
+
+
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+
+    private class LNode {
+        public LNode prev;
+        public T item;
+        public LNode next;
+
+        LNode(T i, LNode n) {
+
+            item = i;
+            next = n;
+//            n.prev=this;
+        }
+
+//        LNode(LNode p,T i)
+//        {
+//            prev=p;
+//            p.next=this;
+//            item=i;
+//        }
+
+        /** ...
+         * @param  p p is the previos node of this node
+         * @param i i is the item of this node
+         * @param n n is the next node of this  node
+        * */
+        /* this node is behind p and before n */
+         LNode(LNode p, T i, LNode n) {
+            prev = p;
+            p.next = this;
+            item = i;
+            next = n;
+            n.prev = this;
+
+        }
+
+    }
+
+
+    private LNode sentFront;
+    private LNode sentBack;
+    private  int size_;
+
+     public void initSentinel() {
+        sentFront = new LNode(null, null);
+        sentBack = new LNode(null, null);
+        sentFront.next = sentBack;
+        sentBack.prev = sentFront;
+    }
+    LinkedListDeque61B() {
+        size_=0;
+        initSentinel();
+    }
+
+    @Override
+    public void addFirst(T x) {
+        new LNode(sentFront,x,sentFront.next);
+    }
+
+    @Override
+    public void addLast(T x) {
+        new LNode(sentBack.prev,x,sentBack);
+    }
+
+    @Override
+    public List<T> toList() {
+
+         List<T> ls=new ArrayList<>();
+         LNode p =sentFront.next;
+         while(p!=sentBack)
+         {
+             ls.add(p.item);
+             p=p.next;
+
+         }
+        return ls;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size_==0;
+    }
+
+    @Override
+    public int size() {
+        return size_;
+    }
+
+    @Override
+    public T removeFirst() {
+         LNode p=sentFront.next;
+         sentFront.next=p.next;
+         p.prev=sentFront;
+        return p.item;
+    }
+
+    @Override
+    public T removeLast() {
+        LNode p=sentBack.prev;
+        sentBack.prev=p.prev;
+        p.next=sentBack;
+        return p.item;
+    }
+
+    /**...
+     * @param index default start by 1
+    * */
+    @Override
+    public T get(int index) {
+        if(index>size_||index <=0)
+        {
+            throw new IndexOutOfBoundsException("Please give a valid index of this deque");
+
+        }
+        T res=null;
+       int cnt=0;
+       if(2*index<size_)
+       {
+
+           LNode p=sentFront;
+           while(cnt!=index)
+           {
+               p=p.next;
+               cnt++;
+           }
+           res=p.item;
+       }
+       else
+       {
+           LNode p=sentBack;
+           while (cnt!=index)
+           {
+               p=p.prev;
+               cnt++;
+           }
+           res=p.item;
+       }
+
+       return res;
+    }
+
+    /*》》》》》》》》》》》》》》》》》 How to understand the Iterator() and a class of
+    * Iterator that implements the hasNext() and next()
+    *
+    * from the course we can deduce the left code is equal to right code ( see lec11 9:59 )
+    * And that means we should have the iterator() method to return an Iterator Object
+    * that Iterator object is only associated with this class (you can define it whatever you want
+    *  but we recommend use a class in Parent class and implements the Iterator, we just return a new Object of
+    * this specific iterator and implements the hasNext() and next() in the class.
+    *
+    * The equivalence will automatically call the iterator method and do what like the right code.
+    *
+    * but first you need to extends the iterable to let java know you are iterable whether it won't
+    * change the for each loop automatically to the iterator!
+    * */
+    private  class SpecificIterator implements Iterator<T>
+    {
+        private  LNode wizPos ;
+        SpecificIterator()
+        {
+            wizPos=sentFront.next;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return wizPos!=sentBack;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            T returnItem =wizPos.item;
+            wizPos=wizPos.next;
+            return returnItem;
+        }
+    }
+ /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new SpecificIterator();
+    }
+
+    @Override
+    public  String toString()
+    {
+
+    }
+
+    @Override
+    public  boolean equals(Object obj)
+    {
+
+    }
+
+    @Override
+    public T getRecursive(int index) {
+
+        return null;
+    }
 }
+
